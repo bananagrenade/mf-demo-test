55479555ad07b771f3e9465f158008bede0184f6
bananagrenade <abraham.chiu@yahoo.com>
2021-12-24 13:08:03 -0600

    chore

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  cache: false,
+
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: false,
+  },
+
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        exclude: /node_modules/,
+        options: {
+          presets: [require.resolve('@babel/preset-react')],
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
