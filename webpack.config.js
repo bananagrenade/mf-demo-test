997bd8632156a643dff733defcfd09ba069187e4
bananagrenade <abraham.chiu@yahoo.com>
2021-12-24 13:15:07 -0600

    omething

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
 
 module.exports = {
   entry: './src/index',
@@ -8,6 +10,10 @@ module.exports = {
 
   optimization: {
     minimize: false,
+  },
+
+  output: {
+    publicPath: 'http://localhost:3001/',
   },
 
   resolve: {
@@ -28,6 +34,22 @@ module.exports = {
   },
 
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'app1',
+      filename: 'remoteEntry.js',
+      remotes: {
+        app2: "app2@http://localhost:3002/remoteEntry.js",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+        },
+        'react-dom': {
+          singleton: true,
+        },
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
