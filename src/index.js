c13629f1a8e412c92352d5ddbff869160cd18338
bananagrenade <abraham.chiu@yahoo.com>
2021-12-24 13:42:58 -0600

    chore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import('./bootstrap');
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+      <div>
+        Welcome to Host App
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
